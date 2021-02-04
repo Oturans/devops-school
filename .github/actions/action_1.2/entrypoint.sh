a6736ac51f7193c76eb40ec0b6a38e62fd4dac04
Andrey Protasovitskiy <61847253+Oturans@users.noreply.github.com>
2021-02-04 13:00:59 +0300

    Update entrypoint.sh

--- .github/actions/action_1.2/entrypoint.sh
+++ .github/actions/action_1.2/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -l
 
-echo "Enter something $1"
-echo "$1" >> log.md
-
 echo "EXECUTING PYTHON SCRIPT..."
 python3 /main.py
 
@@ -14,3 +11,6 @@ pwd
 
 echo "ls -lah /github/workspace"
 ls -lah /github/workspace
+
+echo "Enter something $1"
+echo "$1" >> log.md
